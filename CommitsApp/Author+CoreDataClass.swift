1eaff2858cf1225e6e9a0b2ab2e7762a78e64e17
Max Guraevskiy <58175052+MaxGuraevskiy@users.noreply.github.com>
2020-11-30 02:58:36 +0300

    Add files via upload
    Homeworks 30.11.20 2:58

---
+++ CommitsApp/Author+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Author+CoreDataClass.swift
+//  CommitsApp
+//
+//  Created by Guraevskiy Max on 30.11.2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Author)
+public class Author: NSManagedObject {
+
+}
